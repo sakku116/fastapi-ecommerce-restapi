250bfd07602392ce3cf66691a7dff5b8bcd9d986
zakky <zakky1164@gmail.com>
2024-11-06 16:47:07 +0700

    feat(auth): inherit base token resp in login, refresh-token, register endpoint response and pass it in data field at the same time

--- domain/rest/auth_rest.py
+++ domain/rest/auth_rest.py
@@ -1,8 +1,23 @@
-from pydantic import BaseModel, field_validator
-from .generic_resp import RespData
-from domain.dto import auth_dto
 from dataclasses import dataclass
+
 from fastapi import Form
+from pydantic import BaseModel, field_validator
+
+from domain.dto import auth_dto
+
+from .generic_resp import RespData
+
+
+class BaseTokenResp(BaseModel):
+    """
+    !!IMPORTANT!!
+    fastapi oauth need `access_token` field in the root of the response,
+    also my mobile client GUY need it inside of the `data` field.
+    so this base token resp model need to be inherited to response model and passed in `data` field at the same time
+    """
+    access_token: str
+    refresh_token: str
+
 
 @dataclass
 class LoginReq:
@@ -10,17 +25,18 @@ class LoginReq:
     password: str = Form()
 
 
-class LoginResp(RespData):
-    access_token: str
-    refresh_token: str
+class LoginResp(RespData, BaseTokenResp):
+    pass
+
 
 @dataclass
 class RefreshTokenReq:
     refresh_token: str = Form()
 
-class RefreshTokenResp(RespData):
-    access_token: str
-    refresh_token: str
+
+class RefreshTokenResp(RespData, BaseTokenResp):
+    pass
+
 
 @dataclass
 class RegisterReq:
@@ -30,13 +46,15 @@ class RegisterReq:
     password: str = Form()
     confirm_password: str = Form()
 
-class RegisterResp(RespData):
-    access_token: str
-    refresh_token: str
+
+class RegisterResp(RespData, BaseTokenResp):
+    pass
+
 
 @dataclass
 class CheckTokenReq:
     access_token: str = Form()
+
 
 class CheckTokenRespData(auth_dto.CurrentUser):
     pass
