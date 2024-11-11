efc3e30e1a64ed67961dd6ed8d27d9aa80b14d3b
sakku116 <kycraft1164@gmail.com>
2024-11-11 17:25:56 +0700

    feat(model): add private attribute getter classmethods in base model

--- domain/model/base_model.py
+++ domain/model/base_model.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PrivateAttr
+from pydantic.fields import ModelPrivateAttr
 from bson.int64 import Int64
 
 
@@ -15,8 +16,8 @@ class MyBaseModel(BaseModel):
     """
     id field already indexed by default, but it need to be indexed manually if you set the _indexes field.
     """
-
-    _coll_name: str = ""
+    _coll_name: ModelPrivateAttr = PrivateAttr("")
+    _bucket_name: ModelPrivateAttr = PrivateAttr("")
     _default_indexes: list[_MyBaseModel_Index] = [
         _MyBaseModel_Index(keys=[("id", 1)], unique=True)
     ]
@@ -36,3 +37,24 @@ class MyBaseModel(BaseModel):
             if field in data:
                 data[field] = Int64(data[field])
         return data
+
+
+    @classmethod
+    def getCollName(cls):
+        return cls._coll_name.get_default()
+
+    @classmethod
+    def getDefaultIndexes(cls):
+        return cls._default_indexes.get_default()
+
+    @classmethod
+    def getCustomIndexes(cls):
+        return cls._custom_indexes.get_default()
+
+    @classmethod
+    def getDefaultInt64Fields(cls):
+        return cls._default_int64_fields.get_default()
+
+    @classmethod
+    def getCustomInt64Fields(cls):
+        return cls._custom_int64_fields.get_default()
