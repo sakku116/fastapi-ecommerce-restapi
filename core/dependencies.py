fa16bb6646fa3e5d016a23667d9e5e2a1d899e83
sakku116 <kycraft1164@gmail.com>
2024-11-12 11:43:59 +0700

    feat: create adminOnly dependency

--- core/dependencies.py
+++ core/dependencies.py
@@ -10,14 +10,25 @@ import json
 from typing import Optional
 from core.logging import logger
 from domain.dto import auth_dto
+from core.exceptions.http import CustomHttpException
 
-reusable_token = OAuth2PasswordBearer(
-    "/auth/login"
-)
+reusable_token = OAuth2PasswordBearer("/auth/login")
+
 
 async def verifyToken(
     auth_service: auth_service.AuthService = Depends(),
     token: str = Depends(reusable_token),
 ) -> auth_dto.CurrentUser:
     current_user = auth_service.verifyToken(token=token)
+    return current_user
+
+
+async def adminOnly(
+    current_user: auth_dto.CurrentUser = Depends(verifyToken),
+) -> auth_dto.CurrentUser:
+    if current_user.role != "admin":
+        exc = CustomHttpException(status_code=401, message="Unauthorized")
+        logger.error(exc)
+        raise exc
+
     return current_user
