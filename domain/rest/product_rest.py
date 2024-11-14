b403e2aff7ed8dfa25c4a6450b129b6468906e77
zakky <kycraft1164@gmail.com>
2024-11-14 13:07:08 +0700

    feat(dto): create BaseProductResp model

--- domain/rest/product_rest.py
+++ domain/rest/product_rest.py
@@ -1,18 +1,51 @@
 from pydantic import BaseModel
 from typing import Optional, Literal
-from domain.model import product_model
+from domain.model import product_model, base_model
+from minio import Minio
+from utils import helper
+
+
+class BaseProductResp(base_model.MinioUtil):
+    _bucket_name = product_model.ProductModel.getBucketName()
+    _minio_fields = product_model.ProductModel.getMinioFields()
+    id: str = ""
+    name: str = ""
+    price: float = 0
+    localized_price: str = ""
+    img: Optional[str] = None
+
+    def asResponse(
+        self,
+        minio_client: Optional[Minio] = None,
+        currency_code: Optional[str] = None,
+        language_code: Optional[str] = None,
+    ):
+        """
+        - localize price in localized_price field
+        - convert img to minio url instead filename
+        """
+
+        if self.price and currency_code and language_code:
+            self.localized_price = helper.localizePrice(self.price, currency_code, language_code)
+        if self.img:
+            self.urlizeMinioFields(minio_client=minio_client)
+
 
 class GetProductListReq(BaseModel):
     category_id: Optional[str] = None
     query: Optional[str] = None
     query_by: Optional[Literal[product_model.QUERIABLE_FIELDS_ENUMS]] = None
-    sort_by: Literal[product_model.SORTABLE_FIELDS_ENUMS] = product_model.SORTABLE_FIELDS_ENUMS_DEF
+    sort_by: Literal[product_model.SORTABLE_FIELDS_ENUMS] = (
+        product_model.SORTABLE_FIELDS_ENUMS_DEF
+    )
     sort_order: Literal["asc", "desc"] = "desc"
     page: int = 1
     limit: int = 10
 
-class GetProductListRespDataItem(BaseModel):
-    name: str = ""
-    price: float = 0
-    localized_price: str = ""
-    img: Optional[str] = None
+
+class GetProductListRespDataItem(BaseProductResp):
+    pass
+
+
+class GetProductDetailRespData(product_model.ProductModel, BaseProductResp):
+    pass
