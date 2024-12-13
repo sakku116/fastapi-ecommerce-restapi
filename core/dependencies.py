bfe7df061bb15c992895ea4be93f5e729986d4e7
zakky <kycraft1164@gmail.com>
2024-12-13 18:00:53 +0700

    fix(auth): login doesnt work because formOrJson func dont accept forn url encoded

--- core/dependencies.py
+++ core/dependencies.py
@@ -41,7 +41,7 @@ def formOrJson(model: Type[_TModel]) -> _TModel:
         type_ = request.headers["Content-Type"].split(";", 1)[0]
         if type_ == "application/json":
             data = await request.json()
-        elif type_ == "multipart/form-data":
+        elif type_ == "multipart/form-data" or type_ == "application/x-www-form-urlencoded":
             data = await request.form()
         else:
             raise CustomHttpException(status_code=415, message="Unsupported Media Type")
